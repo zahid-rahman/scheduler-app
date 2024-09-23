f72c1d62f270602a6aa1ebe835f8dddf17d04db0
zahid-rahman <rahmanzahid94@gmail.com>
2024-09-23 09:30:30 +0600

    feat: add initial svelte app for testing

---
+++ sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<header>
+    <h1>Scheduler.io</h1>
+    <ul style="background-color: #C2185B; border-radius: 10px;">
+        <li style="display: inline-block; padding: 10px"><a style="text-decoration: none; color: #ffffff; text-transform: capitalize" href="/">home</a></li>
+        <li style="display: inline-block; padding: 10px"><a style="text-decoration: none; color: #ffffff; text-transform: capitalize" href="/about">about</a></li>
+        <li style="display: inline-block; padding: 10px"><a style="text-decoration: none; color: #ffffff; text-transform: capitalize" href="/profile/100">My profile</a></li>
+    </ul>
+</header>
+
+<div style="height: 80vh">
+    <slot />
+</div>
+
+<footer>
+    <div style="display: block; backgorund-color: #E91E63; width: 100%;">
+        <a style="color: #000000; text-decoration: none" href="/">scheduler.io</a>
+    </div>
+</footer>
---
+++ sveltekit/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { goto } from '$app/navigation';
+
+    function handleClick () {
+        console.log('clicked');
+        goto('/about')
+    }
+</script>
+
+<h2>Home page</h2>
+
+<button on:click={handleClick}>go to about page</button>
